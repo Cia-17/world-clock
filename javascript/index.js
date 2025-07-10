4df3efc7db73a47be881900758f0b491d1364889
Cia-17 <kyeibaffourcia@gmail.com>
2025-07-10 11:54:19 -0700

    Add link to all cities and removed seconds

--- javascript/index.js
+++ javascript/index.js
@@ -40,10 +40,11 @@ function updateCity(event) {
       <h2>${cityName}</h2>
       <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    <div class="time">${cityTime.format("h:mm")} <small>${cityTime.format(
     "A"
   )}</small></div>
   </div>
+  <a href="/">Back</a>
   `;
 }
 
